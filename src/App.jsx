174bb81f6be4922b395ab6c54afcb785ee8c3784
Sergey <sergsmile29@gmail.com>
2024-03-25 23:27:11 +0200

    init m2_styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Button from './components/Button/Button'
 import Header from './components/header/Header'
 import List from './components/list/List'
 import Profile from './components/profile/Profile'
-import TextBox from './components/textBox/TextBox'
 import userData from './assets/user.json'
 
 const App = () => {
-	const titleBtn = 'Please click me'
 	const filmsData = [
 		{ id: '1', title: 'GoT' },
 		{ id: '2', title: 'Harry Potter' },
@@ -23,26 +20,13 @@ const App = () => {
 	]
 
 	const online = false
-	const selectedTab = 'films'
 
 	return (
 		<div>
 			<Header />
 			<Profile user={userData} />
-			{/* Рендер за умовою */}
-			{online && <h1>Welcome!</h1>}
-			{/* Тернарний умовний рендер! */}
-			{online ? <h2>Hello bro!</h2> : <h2>Please login!</h2>}
-
-			<TextBox text='Hello world' />
-			<Button title='Submit' />
-			<Button title={titleBtn} count={22} />
-
-			{selectedTab === 'films' ? (
-				<List online={online} data={filmsData} title='Movies' />
-			) : (
-				<List data={todosData} title='Todos section' />
-			)}
+			<List online={online} data={filmsData} title='Movies' />
+			<List data={todosData} title='Todos section' />
 		</div>
 	)
 }
