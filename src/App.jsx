7b25411ae5c60f016c3cf192660deae817881e43
Sergey <sergsmile29@gmail.com>
2024-03-28 11:52:19 +0200

    modal logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from 'react'
 import { ColorPicker } from './components/ColorPicker/ColorPicker'
 import { Counter } from './components/Counter/Counter'
+import Modal from './components/Modal/Modal'
 import { TodoList } from './components/TodoList/TodoList'
+import Button from './components/Button/Button'
 
 const App = () => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const openModal = () => setIsOpen(true)
+	const closeModal = () => setIsOpen(false)
+	// const toggleModal = () => setIsOpen(prev => !prev)
+
+	// useEffect(() => {
+	// 	setTimeout(() => {
+	// 		openModal()
+	// 	}, 10000)
+	// }, [])
 	return (
 		<>
 			{/* <Counter /> */}
 			{/* <ColorPicker /> */}
 			<TodoList />
+			<Button onClick={openModal}>Open modal</Button>
+			{isOpen && (
+				<Modal closeModal={closeModal}>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit asperiores molestiae quidem, pariatur sunt
+						soluta commodi ut officia ab, accusamus esse mollitia? Nulla molestias suscipit incidunt unde harum commodi
+						perspiciatis.
+					</p>
+				</Modal>
+			)}
 		</>
 	)
 }
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,14 +1,53 @@
+import { useEffect } from 'react'
 import Button from '../Button/Button'
 import s from './Modal.module.css'
-const Modal = ({ children, title = 'Default modal' }) => {
+const Modal = ({ children, title = 'Default modal', closeModal }) => {
+	useEffect(() => {
+		console.log('Модалка відкрилась')
+		const handleKeyDown = e => {
+			console.log(e.key)
+			if (e.key === 'Escape') {
+				closeModal()
+			}
+		}
+
+		document.addEventListener('keydown', handleKeyDown)
+
+		const intervalId = setInterval(() => {
+			// console.log(new Date().toLocaleTimeString())
+		}, 1000)
+
+		const timeoutId = setTimeout(() => {
+			console.log('Hello')
+		}, 3000)
+
+		return () => {
+			console.log('Модалка закрита!')
+			clearInterval(intervalId)
+			clearTimeout(timeoutId)
+			removeEventListener('keydown', handleKeyDown)
+			console.log('Час зупинено')
+		}
+	}, [closeModal])
+
+	const handleBackdropClick = e => {
+		// console.log(e.target)
+		// console.log(e.currentTarget)
+		if (e.target === e.currentTarget) {
+			closeModal()
+		}
+	}
+
 	return (
-		<div className={s.wrapper}>
+		<div className={s.wrapper} onClick={handleBackdropClick}>
 			<div className={s.content}>
 				<>
 					<h1>{title}</h1>
 					<hr />
 				</>
-				<Button className={s.closeBtn}>×</Button>
+				<Button onClick={closeModal} className={s.closeBtn}>
+					×
+				</Button>
 				{children}
 			</div>
 		</div>
