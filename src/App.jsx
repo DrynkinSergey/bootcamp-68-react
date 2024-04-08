4b755c757c264c2a4a3eefdec375ad86c72206fc
Sergey <sergsmile29@gmail.com>
2024-04-08 10:19:14 +0300

    add routes and browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Routes, Route, Link } from 'react-router-dom'
 const App = () => {
-	return <div></div>
+	return (
+		<div>
+			{/* <a href='/'>Home</a>
+			<a href='/about'>About</a> */}
+			<Link to='/'>Home</Link>
+			<Link to='/about'>About</Link>
+			<Routes>
+				<Route path='/' element={<h1>Home</h1>} />
+				<Route path='/about' element={<h1>About</h1>} />
+
+				<Route path='*' element={<h1>Not found</h1>} />
+			</Routes>
+		</div>
+	)
 }
 
 export default App
