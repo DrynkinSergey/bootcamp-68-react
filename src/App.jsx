eb6d20254ce31780a4459c425487084475f918a6
Sergey <sergsmile29@gmail.com>
2024-04-08 10:59:58 +0300

    users page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import Layout from './components/Layout'
+import Users from './pages/Users/Users'
 const App = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Layout />}>
 				<Route index element={<Home />} />
 				<Route path='about' element={<About />} />
+				<Route path='users' element={<Users />} />
 			</Route>
 			<Route path='*' element={<NotFound />} />
 		</Routes>
---
+++ src/pages/Users/Users.jsx
@@ -0,0 +1,15 @@
+import s from './Users.module.css'
+const Users = () => {
+	return (
+		<div>
+			<h1>Users page</h1>
+			<ul>
+				<li>user</li>
+				<li>user</li>
+				<li>user</li>
+			</ul>
+		</div>
+	)
+}
+
+export default Users
