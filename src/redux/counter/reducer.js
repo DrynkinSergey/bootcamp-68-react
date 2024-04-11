43098322203b6b77bcdbf462905470387ecabc67
Sergey <sergsmile29@gmail.com>
2024-04-11 10:21:30 +0300

    configureStore

--- src/redux/counter/reducer.js
+++ src/redux/counter/reducer.js
@@ -1,32 +1,50 @@
 import { changeStep, decrement, increment, reset } from './actions'
+import { createReducer } from '@reduxjs/toolkit'
 
 const initialState = {
 	counter: 0,
 	step: 1,
 }
 
-export const counterReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case increment.type:
-			return {
-				...state,
-				counter: state.counter + state.step,
-			}
-		case decrement.type:
-			return {
-				...state,
-				counter: state.counter - state.step,
-			}
-		case reset.type:
-			return initialState
+export const counterReducer = createReducer(initialState, builder => {
+	builder
+		.addCase(increment, (state, action) => {
+			state.counter += state.step
+		})
+		.addCase(decrement, (state, action) => {
+			state.counter -= state.step
+		})
+		.addCase(reset, (state, action) => {
+			state.counter = 0
+			state.step = 1
+		})
+		.addCase(changeStep, (state, { payload }) => {
+			state.step = payload
+		})
+})
 
-		case changeStep.type:
-			return {
-				...state,
-				step: action.payload,
-			}
+// export const counterReducer = (state = initialState, action) => {
+// 	switch (action.type) {
+// 		case increment.type:
+// 			return {
+// 				...state,
+// 				counter: state.counter + state.step,
+// 			}
+// 		case decrement.type:
+// 			return {
+// 				...state,
+// 				counter: state.counter - state.step,
+// 			}
+// 		case reset.type:
+// 			return initialState
 
-		default:
-			return state
-	}
-}
+// 		case changeStep.type:
+// 			return {
+// 				...state,
+// 				step: action.payload,
+// 			}
+
+// 		default:
+// 			return state
+// 	}
+// }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,15 @@
-import { createStore, combineReducers } from 'redux'
 import { counterReducer } from './counter/reducer'
-import { devToolsEnhancer } from '@redux-devtools/extension'
 import { todosReducer } from './todolist/reducer'
 import { booksReducer } from './books/reducer'
+import { configureStore } from '@reduxjs/toolkit'
 
-const enhancer = devToolsEnhancer()
-
-const rootReducer = combineReducers({
-	todos: todosReducer,
-	counter: counterReducer,
-	books: booksReducer,
+// Створюємо стор та передаємо обов'язково reducer.
+// Якщо використовуємо в проекті 1 редьюсер, передаємо як один
+// Якщо декілька - то у вигляді об'єкта
+export const store = configureStore({
+	reducer: {
+		todos: todosReducer,
+		counter: counterReducer,
+		books: booksReducer,
+	},
 })
-
-export const store = createStore(rootReducer, enhancer)
